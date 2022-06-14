2e2ed79801997206e2de48a4f49aca6c8dba24f0
ravinu <ravinu.gcom@gmail.com>
2022-06-14 17:03:37 +0530

    created Less_animations.py

---
+++ Less_animations.py
@@ -0,0 +1,263 @@
+import itertools
+import os
+import threading
+import time
+import sys
+Permission = "y"
+guess = ""
+guess_count = 2
+guess_limit = 3
+out_of_guesses = False
+screen_clear = False
+import random
+m = random.randint(5,22)
+num = random.randint(1,5)
+database_versiob = m/10
+database_ver = round(database_versiob)
+n = random.randint(0,7)
+randox = random.randint(1,11)
+r = round(randox)
+q1 = "com.root.s5"
+q2 = "com.root.s3"
+q3 = "com.root.s0"
+q4 = "com.root.s5"
+q5 = "com.root.s2"
+q6 = "com.root.s9"
+q7 = "com.root.s17"
+if os.path.exists("log.txt"):
+             os.remove("log.txt")
+else:
+              print("The file does not exist")
+
+while guess != Permission and not (out_of_guesses):
+    if guess_count < guess_limit:
+        print("UAC Prompt:")
+        guess = input("Type [y] To Allow Or [n] To Decline: ")
+        guess_count += 1
+    else:
+        exit()
+if out_of_guesses:
+    print("Application access was Declined")
+else:
+    print("Permission was given!")
+    print("The Application Will Now Continue")
+
+    import time
+
+    time.sleep(1.4)
+    print("Establishing Secure Connection To Database")
+
+    time.sleep(0.5)
+    if n == 1:
+     print(q1)
+    elif n == 2:
+        print(q2)
+    elif n == 3:
+        print(q3)
+    elif n == 4:
+        print(q4)
+    elif n == 5:
+        print(q5)
+    else:
+        print(q7)
+
+    import time
+
+    time.sleep(1.4)
+    print("Succesfully connected to databases")
+        
+    time.sleep(1.4)
+    print("Updating Databases")
+
+    import time
+
+    time.sleep(1.0)
+    print("Locating assets")
+
+if out_of_guesses:
+    screen_clear
+else:
+
+    import itertools
+    import threading
+    import time
+    import sys
+
+    done = False
+
+    #here is the animation
+    def animate():
+        print('\rChecking For Updates ')
+        for c in itertools.cycle(['.', '.']):
+            if done:
+                break
+            sys.stdout.write(c)
+            sys.stdout.flush()
+            time.sleep(0.1)
+
+    t = threading.Thread(target=animate)
+    t.start()
+
+    #long process here
+    time.sleep(10)
+    done = True
+
+if out_of_guesses:
+    screen_clear
+else:
+
+    import time
+
+    time.sleep(0.1)
+    print("")
+    print("Updating Application")
+
+    import time
+
+    time.sleep(0.1)
+    print("This Proccess Will Take About 5-10sec")
+
+    import time
+
+    time.sleep(0.1)
+    print("Updating Application Modules")
+
+    import time
+
+    time.sleep(2.0)
+    print("Application Modules Are Updated")
+
+    import time
+
+    time.sleep(0.1)
+    print("Updating Application Files And Folders")
+
+    import time
+
+    time.sleep(1.1)
+    print("Application File And Folders Are Updated")
+
+    import time
+
+    time.sleep(0.1)
+    print("Upgrading Database To Version ", database_ver)
+
+    import time
+
+    time.sleep(3.0)
+    print(
+        "Application Is Successfully Updated. App version: ", r)
+    print("Database version: ", database_ver)
+
+if out_of_guesses:
+    screen_clear
+else:
+
+    
+
+    done = False
+    '|', '/', '-', '\\'
+
+    def animate():
+        print('\rScanning Your PC ')
+        for c in itertools.cycle(['.', '.']):
+            if done:
+                break
+            sys.stdout.write(c)
+            sys.stdout.flush()
+            time.sleep(0.1)
+        print("")
+        print("Viruses Found")
+
+    t = threading.Thread(target=animate)
+    t.start()
+
+    #long process here
+    time.sleep(10)
+    done = True
+
+if out_of_guesses:
+    screen_clear
+else:
+
+    import time
+    print("")
+    time.sleep(2.4)
+    print("")
+
+if out_of_guesses:
+    screen_clear
+else:
+
+    import itertools
+    import threading
+    import time
+    import sys
+
+    done = False
+    '|', '/', '-', '\\'
+    print('\rObtaining More Infomation ')
+    def animate():
+        for c in itertools.cycle(['.', '.']):
+            if done:
+                break
+            sys.stdout.write(c)
+            sys.stdout.flush()
+            time.sleep(0.1)
+
+    t = threading.Thread(target=animate)
+    t.start()
+
+    #long process here
+    time.sleep(2)
+    done = True
+print("")
+print("The Virus Was Removed. Run this program again later")
+ab = input("Would you like to see the log? [y] or [n]: ")
+print("(C)2021 - 1to5PC (Log Version 5.3.2)")
+if ab == 'y':
+
+    import time
+    time.sleep(0.1)
+    print("")
+    time.sleep(0.1)
+    print("The virus was removed from the following location(s)")
+    print("")
+    time.sleep(2.4)
+    if num == 1:
+     print("dir: C:/.hidden")
+     time.sleep(0.1)
+     print("")
+     print("dir: %appdata%.hidden/recover")
+     time.sleep(0.1)
+     print("")
+     time.sleep(2.4)
+    elif num == 2:
+      print("dir: C:/users/public/temp.folder/vira/a")
+      time.sleep(0.1)
+      print("")
+      print("dir: :External.A-D")
+      time.sleep(0.1)
+      print("")
+      time.sleep(2.4)
+    elif num == 3:
+       print("dir: C:")
+       time.sleep(0.1)
+       print("")
+       print("dir: :External.A-D")
+       time.sleep(0.1)
+       print("")
+       time.sleep(2.4)
+    elif num == 4:
+         print("dir: win.ext/temp/vira")
+         time.sleep(0.1)
+         print("")
+         print("dir: %appdata%/vira/a")
+         time.sleep(0.1)
+         print("")
+         time.sleep(2.4)
+    else:
+          print("Log was unable to recover file paths")
+          time.sleep(0.1)
+          print("")
+          time.sleep(2.4)
